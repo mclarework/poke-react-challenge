c3494b490014044dc463996c7a1ce831ea3abb7a
John Muir <johnmuir1907@gmail.com>
2020-02-25 11:48:51 +0000

    max of 4 selected

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -2,11 +2,12 @@ import React from "react";
 import "../infoCard.css";
 
 const InfoCard = props => {
-  console.log(props.pokemonData);
+  // console.log(props.pokemonData);
   let arr = props.pokemonData.name.charAt(0).toUpperCase() + props.pokemonData.name.slice(1)
   return (
     <div className="card">
       <img src={props.pokemonData.sprites.front_default} alt="pokemon character"/>
+      <img className="deleteIcon" onClick={() => props.click(props.label)} src="https://img.icons8.com/cotton/64/000000/delete-sign--v1.png" alt="delete"></img>
       <h3>Name: {arr}</h3>
       <h3>Height: {props.pokemonData.height}</h3>
       <h3>Abiltities</h3>
