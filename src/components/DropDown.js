b9f10376b6a97e70c63d19ba9f64e3a76cf04ba0
CN18 <codenation@CN18s-MacBook-Pro.local>
2020-02-25 13:37:59 +0000

    Made some improvements to the display box

---
+++ src/components/DropDown.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../DropDown.css";
+
+const DropDown = (props) => {
+    return (
+        <div className="drop-down">
+        {props.suggested ? (
+          props.suggested.map((pokemon, index) => {
+            return (
+              <h3 key={index} onClick={() => props.handleInputClick(pokemon.name, index)}>
+                {pokemon.name}
+              </h3>
+            );
+          })
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
+    )
+}
+
+export default DropDown
