3c7c7e9e7585ba2561c96108c55152fa61db0204
CN18 <codenation@CN18s-MacBook-Pro.local>
2020-02-26 15:47:00 +0000

    Updates

---
+++ precache-manifest.48c868768ff0680b0695c3175c75ca0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e7e2891275dfe9dbb076c12ab9ed83",
+    "url": "/poke-react-challenge/index.html"
+  },
+  {
+    "revision": "5a6ade68d241b88867a4",
+    "url": "/poke-react-challenge/static/css/main.64c2db68.chunk.css"
+  },
+  {
+    "revision": "ad14f3b81fb06fb532a3",
+    "url": "/poke-react-challenge/static/js/2.387cb5f6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/poke-react-challenge/static/js/2.387cb5f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6ade68d241b88867a4",
+    "url": "/poke-react-challenge/static/js/main.6396bfc9.chunk.js"
+  },
+  {
+    "revision": "d8a89f42a802ea7a10f5",
+    "url": "/poke-react-challenge/static/js/runtime-main.0e3f5f73.js"
+  },
+  {
+    "revision": "49255cb281084c9c58a3e8c86e743a16",
+    "url": "/poke-react-challenge/static/media/pokemon.49255cb2.png"
+  }
+]);
